811bbc27aca59b5bcb03fd594646ce53c4383e17
mobilezl <zhongliang8080@163.com>
2015-09-21 11:22:22 +0800

    1.crate demo project

---
+++ showDemo/showDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  showDemo
+//
+//  Created by Leo on 15/9/21.
+//  Copyright © 2015年 zhongliang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ showDemo/showDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  showDemo
+//
+//  Created by Leo on 15/9/21.
+//  Copyright © 2015年 zhongliang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
